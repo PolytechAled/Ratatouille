39de1cc286233a038ca7bdb9f60e722852b92a3b
Robin Viollet <90778039+robin-viollet@users.noreply.github.com>
2022-03-03 10:46:51 +0100

    Initial commit

---
+++ app/src/main/java/fr/polytech/ihm/td4menu/ratatouille/MainActivity.java
@@ -0,0 +1,14 @@
+package fr.polytech.ihm.td4menu.ratatouille;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
