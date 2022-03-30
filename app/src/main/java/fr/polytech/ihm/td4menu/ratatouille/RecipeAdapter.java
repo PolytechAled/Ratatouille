dd6ee2b44a1c67b9fc19f733521cf52ba210dc7c
yvars <theo.yvars@gmail.com>
2022-03-30 17:16:25 +0200

    adapt

--- app/src/main/java/fr/polytech/ihm/td4menu/ratatouille/RecipeAdapter.java
+++ app/src/main/java/fr/polytech/ihm/td4menu/ratatouille/RecipeAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.RecyclerView;
 
 public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.ViewHolder> {
